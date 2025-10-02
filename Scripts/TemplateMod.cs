d20ed5da55feebda78eb1ad7cd198b1b91e6ca33
A Corsican Frog <49497194+valentin56610@users.noreply.github.com>
2025-10-02 18:30:21 +0200

    Small refactor
    Refactored some things
    
    Added PhotonUnityNetworking.dll

---
+++ Scripts/TemplateMod.cs
@@ -0,0 +1,36 @@
+﻿using HarmonyLib;
+using UnityEngine;
+
+class ExampleMod : GameModification
+{
+    Harmony _harmony;
+
+    public ExampleMod(Mod p_mod) : base(p_mod)
+    {
+        Debug.Log($"[MOD:{p_mod.name}] Registering...");
+    }
+
+    public override void OnModInitialization(Mod p_mod)
+    {
+        Debug.Log($"[MOD:{p_mod.name}] Initializing...");
+
+        mod = p_mod;
+
+        PatchGame();
+    }
+
+    public override void OnModUnloaded()
+    {
+        Debug.Log($"[MOD:{mod.name}] Unloading...");
+
+        _harmony?.UnpatchAll(_harmony.Id);
+    }
+
+    void PatchGame()
+    {
+        Debug.Log($"[MOD:{mod.name}] Applying Patches...");
+
+        _harmony = new Harmony("com.hexofsteel." + mod.name);
+        _harmony.PatchAll();
+    }
+}
